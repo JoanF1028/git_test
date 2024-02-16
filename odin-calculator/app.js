a4aa6a4a853178949f9417943ea02bfa137bd61e
JoanF1028 <113257225+JoanF1028@users.noreply.github.com>
2024-02-16 10:53:45 -0500

    Add files via upload
    calculator

---
+++ odin-calculator/app.js
@@ -0,0 +1,16 @@
+const output = document.getElementById('output');
+
+function appendToDisplay(input) {
+    output.value += input;
+}
+function clearDisplay() {
+    output.value = '';
+}
+function calculate() {
+    try {
+        output.value = eval(output.value)
+    } catch (error) {
+        output.value = 'Error'
+    }
+    
+}
